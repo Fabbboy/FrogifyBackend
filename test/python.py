b4a2e90ea663ece9c812a556726d6073a63c61a6
fabboy <fabboynice@gmail.com>
2023-03-14 19:40:31 +0100

    Initial commit

---
+++ test/python.py
@@ -0,0 +1,36 @@
+import socket
+import bson
+
+# create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# get local machine name
+host = socket.gethostname()
+
+# bind the socket to a public host, and a port
+server_socket.bind((host, 2121))
+
+# queue up to 5 requests
+server_socket.listen(5)
+
+while True:
+    # establish a connection
+    client_socket, addr = server_socket.accept()
+    print("Got a connection from %s" % str(addr))
+
+    # receive the data sent by the Rust client
+    data = b''
+    while True:
+        chunk = client_socket.recv(4096)
+        if not chunk:
+            break
+        data += chunk
+
+    # decode the BSON data into a Python dictionary
+    decoded_data = bson.loads(data)
+
+    # print the received data
+    print(decoded_data)
+
+    # close the client socket
+    client_socket.close()
